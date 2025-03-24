b77a2fc590c0ee18704710d3d76bcf0052ee569c
Yan Carlos <yan312005@gmail.com>
2025-03-23 23:40:56 -0300

    fix: q.7 logic going from start to end of the array

--- src/q.7.ts
+++ src/q.7.ts
@@ -4,12 +4,12 @@
 
 const arr: Array<number> = [1, 2, 3, 4, 5];
 
-const findFirstOccurency = (item: number, arr: number[], n: number): number => {
-  if (arr[n - 1] === item) return n - 1;
+const findFirstOccurency = (item: number, arr: number[], index: number = 0): number => {
+  if (index >= arr.length) return -1;     
 
-  if (n - 1 === 0) return -1;
-
-  return findFirstOccurency(item, arr, n - 1);
+  if (arr[index] === item) return index;   
+       
+  return findFirstOccurency(item, arr, index + 1); 
 };
 
 console.log(findFirstOccurency(2, arr, arr.length));
